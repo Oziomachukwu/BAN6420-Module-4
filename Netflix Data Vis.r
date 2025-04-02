b2456649fcd1f75736f47d5e73177d1ad5bb0fb0
Oziomachukwu <nevizard@gmail.com>
2025-04-02 04:21:48 +0100

    Update Netflix Data Vis.r

--- Netflix Data Vis.r
+++ Netflix Data Vis.r
@@ -12,7 +12,8 @@ setwd(directory_path)
 cat("Working directory has been set to:", getwd(), "\n")
 
 # Read the dataset
-df <- read.csv("netflix_data_cleaned.csv", stringsAsFactors = FALSE)  # Read without factors
+
+df <- read.csv("netflix_data_cleaned.csv", stringsAsFactors = FALSE)
 
 
 # Convert 'rating' to a factor
@@ -21,9 +22,22 @@ df$rating <- as.factor(df$rating)
 # Create bar plot for ratings distribution
 ggplot(df, aes(x = rating, fill = rating)) +
   geom_bar() +
-  scale_fill_manual(values = c("#E50914","#E50910","#E50908","#E50904","#E50900","#E50920","#E50926","#E50932","#E50936","#E50940","#E50944","#E50948","#E50952","#E50956","#E50960")) +  #  red color scheme
+  scale_fill_manual(values = c("#E50914",
+                               "#E50910",
+                               "#E50908",
+                               "#E50904",
+                               "#E50900",
+                               "#E50920",
+                               "#E50926",
+                               "#E50932",
+                               "#E50936",
+                               "#E50940",
+                               "#E50944",
+                               "#E50948",
+                               "#E50952",
+                               "#E50956",
+                               "#E50960")) +  #  red color scheme
   theme_minimal() +
   labs(title = "Distribution of Content Ratings on Netflix",
        x = "Rating", y = "Count") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels
-
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))# Rotate x-axis labels
